0009076b9db8e1b18a3e5cc83475b847e8e013a0
st_alexisa <sviridov01.alesha@mail.ru>
2022-07-13 01:27:12 +0300

    added Upload command

---
+++ cloudphoto/CloudManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Transfer; 
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace cloudphoto
+{
+    public static class CloudManager
+    {
+        public static bool UploadFiles(AmazonS3Client client, string bucketName, string localDirectoryPath, string cloudAlbumName)
+        {
+            var files = Directory
+                .EnumerateFiles(localDirectoryPath, "*.jpg")
+                .Select(ConvertToUnixTypePath);
+            foreach (var filePath in files)
+            {
+                var fileName = filePath.Split('/').Last();
+                if (!UploadFileToCloud(client, filePath, bucketName, cloudAlbumName + '/' + fileName))
+                    return false;
+            }
+            return true;
+        }
+        
+        private static string ConvertToUnixTypePath(string path)
+        {
+            var builder = new StringBuilder();
+            foreach (var c in path)
+                builder.Append(c != '\\' ? c : '/');
+            return builder.ToString();
+        }
+
+        private static bool UploadFileToCloud(AmazonS3Client client, string localFilePath, string bucketName, string fileNameInCloud)  
+        {
+            var utility = new TransferUtility(client);  
+            var request = new TransferUtilityUploadRequest();  
+
+            request.BucketName = bucketName;
+            request.Key = fileNameInCloud; //file name up in S3  
+            request.FilePath = localFilePath;
+            utility.Upload(request); //commensing the transfer 
+            return true; //indicate that the file was sent  
+        }  
+    }
+}
---
+++ cloudphoto/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Collections;
+using System.Collections.Generic;
+using Amazon.S3.Transfer; 
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace cloudphoto
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length > 6 || args.Length == 0)
+            {
+                Console.WriteLine("Wrong arguments count");
+                return ;
+            }
+            
+            AmazonS3Config configsS3 = new AmazonS3Config { 
+                ServiceURL = "https://s3.yandexcloud.net"
+            };
+            AmazonS3Client client = new AmazonS3Client(configsS3);
+            const string bucketName = "kfu-itis-spr22-cloudphoto";
+            
+            
+            var command = CommandParser.ParseCommand(args[0]);
+            if (command == null)
+            {
+                await Console.Error.WriteLineAsync("unknown command");
+                return ;
+            }
+
+            // if (CloudManager.UploadFiles(client, bucketName, args[4], args[2]))
+            //     Console.WriteLine("success");
+            // else 
+            //     Console.WriteLine("failed");
+            if (TryExecuteCommand(client, bucketName, command, args))
+                Console.WriteLine("Command executed successfully");
+            else 
+                Console.WriteLine("Command execution failed");
+        }
+
+        private static bool TryExecuteCommand(AmazonS3Client client, string bucketName, 
+                CommandParser.CommandType? command, string[] args)
+        {
+            string albumName;
+            string pathName;
+
+            switch (args[1])
+            {
+                case "--album" when args[3] == "--path":
+                    albumName = args[2];
+                    pathName = args[4];
+                    break;
+                case "--path" when args[3] == "--album":
+                    pathName = args[2];
+                    albumName = args[4];
+                    break;
+                default:
+                    Console.Error.WriteLine("Wrong options");
+                    return false;
+            }
+
+            if (command == CommandParser.CommandType.Upload)
+            {
+                if (!CloudManager.UploadFiles(client, bucketName, pathName, albumName))
+                    return false;
+            }
+            
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            return true;
+        }
+    }
+
+    public static class CommandParser
+    {
+        public enum CommandType
+        {
+            Upload,
+            Download,
+            ListAlbums,
+            ListPhotos,
+            GenerateSite
+        }
+
+        public static CommandType? ParseCommand(string cmdString)
+        {
+            if (Enum.TryParse(cmdString, out CommandType command))
+                return command;
+            return null;
+        }
+    }
+}
